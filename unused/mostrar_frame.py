28a3ca31b731d69af292bd3d6daffa24ebafc925
frico <fhrl.eng@gmail.com>
2025-10-24 09:52:40 -0300

    removido alguns arquivos que não estavam sendo usados no código

--- unused/mostrar_frame.py
+++
@@ -1,8 +0,0 @@
-import customtkinter as ctk
-
-def mostrar_frame(app, frame):
-    """Oculta todas as frames e mostra apenas a selecionada"""
-    for f in app.winfo_children():
-        if isinstance(f, ctk.CTkFrame):
-            f.pack_forget()
-    frame.pack(expand=True, fill="both")
